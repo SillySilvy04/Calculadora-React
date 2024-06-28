5ce5c829774b41e92aff3c157a86ee02a5de65ce
silly_silvy <silvyespindola2004@gmail.com>
2024-06-27 23:44:04 -0300

    finalização do design da calculadora

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calculator.css'
 import Container from '@mui/material/Container';
+import { Box } from '@mui/system';
 
-export default function calculator() {
-  return (
-    <Container>
-        <div className="calculator">
-            <div className="display">0</div>
-            <div className="buttons">
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>+</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>*</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
-            <button>/</button>
-            </div>
-        </div>
-    </Container>
-  )
+export default function Calculator() {
+    return (
+        <Box m={5}>
+            <Container maxWidth="xs">
+                <div className="wrapper">
+                    <Box m={12}/>
+                        <h1 className='result'>01</h1>
+                    <button>AC</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button className='orange'>/</button>
+                    <button className='grey'>7</button>
+                    <button className='grey'>8</button>
+                    <button className='grey'>9</button>
+                    <button className='orange'>X</button>
+                    <button className='grey'>4</button>
+                    <button className='grey'>5</button>
+                    <button className='grey'>6</button>
+                    <button className='orange'>-</button>
+                    <button className='grey'>1</button>
+                    <button className='grey'>2</button>
+                    <button className='grey'>3</button>
+                    <button className='orange'>+</button>
+                    <button className='grey'>0</button>
+                    <button className='grey'>,</button>
+                    <button className='grey' style={{ visibility: "hidden" }}>,</button>
+                    <button className='grey'>=</button>
+
+                </div>
+            </Container>
+        </Box>
+    )
 }
